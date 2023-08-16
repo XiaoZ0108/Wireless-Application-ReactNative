523f90b60589214212c84c9b699c236f0d2c7ac6
XiaoZe <Xiaoxiaoze23@gmail.com>
2023-08-16 22:19:00 +0800

    create BigComponent file,move search bar into and create Itembox component

---
+++ BigComponent/ItemBox.js
@@ -0,0 +1,17 @@
+import {Component} from 'react';
+import {StyleSheet} from 'react-native';
+import Card from '../Component/Card.';
+
+class ItemBox extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <Card></Card>;
+  }
+}
+
+export default ItemBox;
+
+const styles = StyleSheet.create({});
